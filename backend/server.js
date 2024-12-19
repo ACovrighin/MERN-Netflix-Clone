aac906b741b1ad11b1a759eba868325e3bc2ffcc
Andrei <andrew44291@gmail.com>
2024-12-19 16:11:59 -0500

    linked API and made movie endpoints, now have to do the same for the TV shows

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import authRoutes from './routes/auth.route.js';
+import movieRoutes from './routes/movie.route.js';
 import {ENV_VARS} from './config/envVars.js';
 import {connectDB} from './config/db.js';
 
@@ -11,8 +12,10 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json()); //will allow parse of req.body obj
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 app.listen(PORT, () => {
     console.log('Server Started at http://localhost:' + PORT);
     connectDB();
 });
+
