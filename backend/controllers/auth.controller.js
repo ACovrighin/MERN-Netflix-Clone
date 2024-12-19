6230b1b0c11cc1f7e6e771df9f62b79b8f91f06a
Andrei <andrew44291@gmail.com>
2024-12-19 12:07:21 -0500

    added backend logic for login and logout, as well as set up JWT tokens and cookies. Next step is fetching movies from API.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import {User} from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { generateTokenAndSetCookie } from '../utils/generateToken.js';
 
 export async function signup(req,res){
     try {
@@ -45,13 +46,19 @@ export async function signup(req,res){
             image
         })
         
-        await newUser.save();
-
-        //Remove password from the response
+        
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+            //Remove password from the response
         res.status(201).json({success:true, user: {
             ...newUser._doc,
             password:""
-        }})
+        },
+    });
+        
+        
+
+        
     } catch (error) {
         console.log("Error in signup controller", error.message)
         res.status(500).json({success:false, message:"Internal server error"})
@@ -59,9 +66,44 @@ export async function signup(req,res){
 }
 
 export async function login (req,res){
-    res.send("Login Route")
+    try {
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({success:false, message:"All fields are required"})
+        }
+
+        const user = await User.findOne({email:email});
+
+        if(!user){
+            return res.status(404).json({success:false, message:"Invalid credentials"})
+        }
+
+        const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+        if(!isPasswordCorrect){
+            return res.status(404).json({success:false, message:"Invalid credentials"})
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+        //Remove password from the response
+        res.status(200).json({success:true, user: {
+            ...user._doc,
+            password:""
+        },
+    });
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({success:false, message:"Internal server error"});
+    }
 }
 
 export async function logout(req,res){
-    res.send("Logout Route")
+    try {
+        res.clearCookie("jwt-netflix");
+        res.status(200).json({success:true, message:"Logged out successfully"})
+    } catch (error) {
+        console.log("Error in logout controller", error.message)
+        res.status(500).json({success:false, message:"Internal server error"})
+    }
 }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,13 +1,15 @@
 import express from "express";
 import {signup} from "../controllers/auth.controller.js"
+import {logout} from "../controllers/auth.controller.js"
+import {login} from "../controllers/auth.controller.js"
 
 const router = express.Router();
 
 router.post("/signup", signup);
 
-router.post("/login", );
+router.post("/login", login);
 
-router.post("/logout", );
+router.post("/logout", logout);
 
 export default router;
 
