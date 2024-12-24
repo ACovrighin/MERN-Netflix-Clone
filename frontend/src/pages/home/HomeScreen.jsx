375185738eb1fbb0956bad74530a536e32ada0cf
Andrei <andrew44291@gmail.com>
2024-12-24 14:59:26 -0500

    started on home screen, navbar started but icons are not in the right place. will come back to it next time and fix.

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { useAuthStore } from '../../store/authUser.js';
+import Navbar from '../../components/Navbar.jsx';
 
 const HomeScreen = () => {
-  const { logout } = useAuthStore();
   return (
-    <div>HomeScreen
-
-      <button onClick={logout}>logout</button>
+    <>
+    <div className='relative h-screen text-white bg-black'>
+      <Navbar />
     </div>
+    
+    </>
   )
 }
 
