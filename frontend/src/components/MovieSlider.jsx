cc61c3ff3e1cb84d03b7b3c35458f14571111327
Andrei <andrew44291@gmail.com>
2025-01-02 11:20:40 -0500

    home page done, watch page next

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -3,7 +3,8 @@ import { useContentStore } from '../store/content.js';
 import axios from 'axios';
 import { useEffect, useRef, useState } from 'react';
 import { SMALL_IMG_BASE_URL } from '../utils/constants.js';
-import { Link } from 'react-router-dom';``
+import { Link } from 'react-router-dom'; ``
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 
 
@@ -18,6 +19,8 @@ const MovieSlider = ({ category }) => {
 
   const [showArrows, setShowArrows] = useState(false);
 
+  const sliderRef = useRef(null);
+
   useEffect(() => {
     const getContent = async () => {
       const res = await axios.get(`/api/v1/${contentType}/${category}`);
@@ -28,20 +31,29 @@ const MovieSlider = ({ category }) => {
     getContent()
   }, [contentType, category]);
 
+  const scrollLeft = () => { 
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({left:-sliderRef.current.offsetWidth, behavior: 'smooth'})
+    }
+  };
+  const scrollRight = () => { 
+    sliderRef.current.scrollBy({left:sliderRef.current.offsetWidth, behavior: 'smooth'})
+  };
+
 
   return <div className='text-white bg-black relative px-5 md:px-20'
-  onMouseEnter={() => setShowArrows(true)}
-  onMouseLeave={() => setShowArrows(false)}
+    onMouseEnter={() => setShowArrows(true)}
+    onMouseLeave={() => setShowArrows(false)}
   >
     <h2 className='mb-4 text-2xl font-bold'>
       {formattedCategoryName} {formattedContentType}
     </h2>
 
-    <div className='flex space-x-4 overflow-x-scroll'>
+    <div className='flex space-x-4 overflow-x-scroll scrollbar-hide' ref={sliderRef}>
       {content.map((item) => (
         <Link to={`/watch/${item.id}`} className='min-w-[250px] relative group' key={item.id}>
           <div className='rounded-lg overflow-hidden'>
-            <img 
+            <img
               src={SMALL_IMG_BASE_URL + item.backdrop_path}
               alt='movie image'
               className='transition-transform duration-300 ease-in-out group-hover:scale-125' />
@@ -52,6 +64,27 @@ const MovieSlider = ({ category }) => {
         </Link>
       ))}
     </div>
+
+    {showArrows && (
+      <>
+        <button
+          className="absolute top-1/2 -translate-y-1/2 left-5 md:left-24 flex items-center justify-center 
+      size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10"
+      onClick={scrollLeft}
+        >
+          <ChevronLeft size={24} />
+          
+        </button>
+
+        <button
+          className="absolute top-1/2 -translate-y-1/2 right-5 md:right-24 flex items-center justify-center 
+      size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10"
+      onClick={scrollRight}
+        >
+          <ChevronRight size={24} />
+        </button>
+      </>
+    )}
 
   </div>;
 };
