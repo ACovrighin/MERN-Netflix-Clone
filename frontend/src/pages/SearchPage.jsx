b6ef5d4e533d9f8892b76d8779474a0bf4d87e10
Andrei <andrew44291@gmail.com>
2025-03-02 14:53:17 -0500

    Still getting a white screen and empty arrays when attempting to search. Gonna take a break and get back to it another time. Search page done other than still needing to fix that error.

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -5,11 +5,12 @@ import axios from 'axios';
 import { toast } from 'react-hot-toast';
 import { ORIGINAL_IMAGE_BASE_URL } from '../utils/constants';
 import { Link } from 'react-router-dom';
+import { Search } from 'lucide-react';
 
 const SearchPage = () => {
-    const[activeTab, setActiveTab] = useState('movies')
-    const[searchTerm, setSearchTerm] = useState('')
-    const[results, setResults] = useState([])
+    const[activeTab, setActiveTab] = useState('movie');
+    const[searchTerm, setSearchTerm] = useState('');
+    const[results, setResults] = useState([]);
     const {setContentType} = useContentStore();
     const handleTabClick = (tab) => {
         setActiveTab(tab);
@@ -33,7 +34,8 @@ const SearchPage = () => {
     console.log("results" , results);
 
 
-  return ( <div className='bg-black min-h-screen text-white'>
+  return (
+     <div className='bg-black min-h-screen text-white'>
     <Navbar />
     <div className='container mx-auto px-4 py-8'>
         <div className='flex justify-center gap-3 mb-4'>
@@ -56,7 +58,7 @@ const SearchPage = () => {
         <form className='flex gap-2 items-stretch mb-8 max-w-2xl mx-auto' onSubmit={handleSearch}>
             <input type='text' placeholder={'Search for a '+ activeTab} className='flex-1 py-2 px-4 rounded bg-gray-800 text-white' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
             <button type='submit' className='py-2 px-4 bg-red-600 hover:bg-red-700 rounded'>
-                Search
+            <Search className='size-6' />
             </button>
         </form>
 
@@ -66,14 +68,14 @@ const SearchPage = () => {
                 return (
                     <div className='bg-gray-800 p-4 rounded'>
                         {activeTab === 'person' ? (
-                            <Link to={"/actor/" + result.name} className='flex flex-col items-center'>
+                            <div className='flex flex-col items-center'>
                                 <img 
                                 src={ORIGINAL_IMAGE_BASE_URL + result.profile_path}
                                 alt={result.name}
                                 className='max-h-96 rounded mx-auto'
                                 />
                                 <h2 className='mt-2 text-xl font-bold'>{result.name}</h2>
-                            </Link>
+                            </div>
                         ) : (
                             <Link to={"/watch/" + result.id} className='flex flex-col items-center'>
                                 <img 
